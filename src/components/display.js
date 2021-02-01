2e43d5053c19402bfe0a281086d6d0af2ee3d556
Fardeen Chowdhury <fhchowdh@edu.uwaterloo.ca>
2021-02-01 16:12:39 -0500

    Added Hind as body font, created section on landing to show JJ and SE

---
+++ src/components/display.js
@@ -0,0 +1,2 @@
+import React from "react"
+import "./display.scss"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Navbar from "../components/navbar"
 import Hero from "../components/hero"
-
+import "../components/main.scss"
 
 const IndexPage = () => {
   return(
