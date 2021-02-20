e462b4f82a409808133deb41418920d082f1452c
Fardeen Chowdhury <fhchowdh@edu.uwaterloo.ca>
2021-02-19 19:10:41 -0500

    Added videos to display

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,19 @@ import React from "react"
 import Navbar from "../components/navbar"
 import Hero from "../components/hero"
 import Display from "../components/display"
+import Community from "../components/community"
 import "../styles/index.scss"
 
 const IndexPage = () => {
   return(
     <>
-      <Navbar />
-      <Hero />
-      <Display />
+    <body>
+        <Navbar />
+        <Hero />
+        <Display game__title="Jumping Jax" game__description="Exploit interesting movement mechanics to chase after your teleporting cyber-corgi and explore a sci-fi city! Use all
+        the tools in your arsenal to bring Jax home before dinner."/>
+        <Community />
+      </body>
     </>
   )
 }
