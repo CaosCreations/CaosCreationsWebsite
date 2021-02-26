ea7aa8d280acc604c167e9614ca5d654dad6c8a1
Daniel Lin <daniel@aikon.com>
2021-02-26 12:34:37 -0800

    Add bg hero image
    - Update hero content to be positioned vertically centered
    - Update navbar to be positioned absolute over the hero
    - Z-index accordingly

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,9 +1,22 @@
 import React from "react"
+import ReactPlayer from 'react-player/youtube'
 import "./hero.scss"
 
 const Hero = () => (
   <div className="hero-container">
-    <div className="hero-internal">
+    <div className="hero-video-wrapper">
+      <ReactPlayer
+        className="hero-video-player"
+        url='https://www.youtube.com/watch?v=OafR9XphiRQ'
+        loop={true}
+        muted={true}
+        playing={true}
+        controls={false}
+        width='100%'
+        height='100%'
+      />
+    </div>
+    <div className="hero-content-wrapper">
       <div className="hero-content">
         <h1 className="tagline">We're a group of ding dongs</h1>
       </div>
