9aef4d0decc19fcdc387f870f9ae831053772381
Daniel Lin <daniel@aikon.com>
2021-03-03 15:49:17 -0800

    Update to vimeo video

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,5 +1,5 @@
 import React from "react"
-import ReactPlayer from 'react-player/youtube'
+import ReactPlayer from 'react-player/vimeo'
 import "./hero.scss"
 
 const Hero = () => (
@@ -7,7 +7,7 @@ const Hero = () => (
     <div className="hero-video-wrapper">
       <ReactPlayer
         className="hero-video-player"
-        url='https://www.youtube.com/watch?v=OafR9XphiRQ'
+        url='https://vimeo.com/519274799'
         loop={true}
         muted={true}
         playing={true}
