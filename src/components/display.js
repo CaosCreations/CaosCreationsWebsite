7f5e69ade753231c355338a4e51f4ca2abf6bae6
Sabien <ambid17@gmail.com>
2021-04-06 02:39:08 -0500

    new videos

--- src/components/display.js
+++ src/components/display.js
@@ -1,11 +1,12 @@
 import React from "react"
 import "./display.scss"
-import sample_clip from "../videos/sample.mp4"
+import se_clip from "../videos/starlightexpress.mp4"
+import jj_clip from "../videos/jumpingjax.mp4"
 
 const Display = (props) => (
     <div className="display-container" id={props.id}>
         <div className="display-video-wrapper">
-            <video className="display-video" preload="auto" autoPlay={true} muted={true} loop={true} src={sample_clip} />
+            <video className="display-video" preload="auto" autoPlay={true} muted={true} loop={true} src={props.isjj === "true" ? jj_clip : se_clip} />
             {/* Need to add static images for browsers that don't load videos properly */}
             <div className="display-content">
                 <p className="display-headline d4">{props.gameTitle}</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,9 @@ const IndexPage = () => {
     <body>
         <Navbar />
         <Hero />
-        <Display id="games" gameTitle="Jumping Jax" gameDescription="Exploit interesting movement mechanics to chase after your teleporting cyber-corgi and explore a sci-fi city! Use all
+        <Display isjj="true" id="games" gameTitle="Jumping Jax" gameDescription="Exploit interesting movement mechanics to chase after your teleporting cyber-corgi and explore a sci-fi city! Use all
         the tools in your arsenal to bring Jax home before dinner." />
-        <Display gameTitle="Starlight Express" gameDescription="Manage your interplanetary delivery company and you expand your fleet as you explore the mysteries of an old space station!"/>
+        <Display isjj="false" gameTitle="Starlight Express" gameDescription="Manage your interplanetary delivery company and you expand your fleet as you explore the mysteries of an old space station!"/>
         <Community />
         <Footer />
     </body>
