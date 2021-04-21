f1b2cdffa3d25d2c98446ac1c7caad3092784152
Daniel <daniellin@berkeley.edu>
2021-04-21 00:12:21 -0700

    Update hero video

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,26 +1,24 @@
-import React from "react"
-import ReactPlayer from 'react-player/vimeo'
-import "./hero.scss"
+import React from "react";
+import heroVideo from "../videos/cc-sphere-square.mp4";
+import "./hero.scss";
 
 const Hero = () => (
   <div className="hero-container">
     <div className="hero-video-wrapper">
-      <ReactPlayer
-        className="hero-video-player"
-        url='https://vimeo.com/519274799'
-        loop={true}
-        muted={true}
-        playing={true}
-        controls={false}
-        width='100%'
-        height='100%'
-      />
+      <video className="hero-video-player"
+            autoplay="true"
+            muted="true"
+            loop="true"
+      >
+        <source src={heroVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
-    <div className="hero-content-wrapper">
-      <div className="hero-content">
-        <p className="tagline d3">We're a group of ding dongs</p>
-      </div>
-    </div>
+        <div className="hero-content-wrapper">
+          <div className="hero-content">
+            <p className="tagline d3">We're a group of ding dongs</p>
+          </div>
+        </div>
   </div>
 )
 
